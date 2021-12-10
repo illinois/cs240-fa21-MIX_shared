ce0a9c9e7c4d9a19b654703fcd9aabc94d25f1ae
duanegroves <dgroves2@illinois.edu>
2021-12-10 15:42:51 -0600

    Took out commented out code and reverted tile.js to deployed code

--- static/tile.js
+++ static/tile.js
@@ -15,27 +15,13 @@ function createTile(element) {
   var temp = document.createElement('div');
   temp.className = "grid-item"
   // Format tile
-  try {
-    temp.innerHTML = '<div class="card"><div class="card-body"><h5 class="card-title">' +
+  temp.innerHTML = '<div class="card"><div class="card-body"><h5 class="card-title">' +
                     element._metadata.tile + 
-                    '</h5>' +
-                    '<img src="' + element.image +'" alt="IM Image">' +
-                    '<p class="card-text">' + 
+                    '</h5><p class="card-text">' + 
                     unpackContent(element) + 
                     '</p><p class="card-text"><small class="text-muted">Created by ' +
                     element._metadata.creator +
                     '</small></p></div></div>';
-  } catch(error) {
-    temp.innerHTML = '<div class="card"><div class="card-body"><h5 class="card-title">' +
-                    element._metadata.tile + 
-                    '</h5>' +
-                    '<p class="card-text">' + 
-                    unpackContent(element) + 
-                    '</p><p class="card-text"><small class="text-muted">Created by ' +
-                    element._metadata.creator +
-                    '</small></p></div></div>';
-  }
-  
   document.getElementById("result").appendChild(temp);
 }
 
